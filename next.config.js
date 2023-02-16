b4bde96ede9fd36cbf5a5caa9e240c849dd8c05b
mpfullstack <info@marcperez.cat>
2023-02-16 17:21:41 +0100

    feat: add styled components support and bundle analyze

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+let nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+const analyzeBundleEnabled = process.env.ANALYZE === 'true';
+if (analyzeBundleEnabled) {
+  const withBundleAnalyzer = require('@next/bundle-analyzer')({ enabled: analyzeBundleEnabled });
+  nextConfig = withBundleAnalyzer(nextConfig);
+};
+
+module.exports = nextConfig;
