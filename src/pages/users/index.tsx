8306164340eee6da1d4e224e3b9e7ec73e44c8ef
mpfullstack <info@marcperez.cat>
2023-02-18 20:57:33 +0100

    fix

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,13 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import useSWR, { Fetcher } from 'swr';
-import { Data } from '../api/users';
-
-const fetcher: Fetcher<Data> = (key: string) => fetch(key).then(res => res.json());
 
 export default function Home() {
-  const { data, isLoading, isValidating } = useSWR('/api/users', fetcher);
-  const isLoadingData = isLoading || isValidating;
   return (
     <>
       <Head>
@@ -21,21 +15,6 @@ export default function Home() {
         <p>
           <Link href="/">{`Back to Home`}</Link>
         </p>
-
-        {isLoadingData && <p>{`Loading matches....`}</p>}
-        {data?.result && (
-          <div>
-            {data?.result.map(user => {
-              return (
-                <div key={user.id}>
-                  <p>{user.id}</p>
-                  <p>{user.name}</p>
-                  <p>{user.nickname}</p>
-                </div>
-              );
-            })}
-          </div>
-        )}
       </main>
     </>
   );
