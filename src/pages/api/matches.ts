3b3977a2b8757f2480af50df10e039ed39d17a60
Marc <marc@hosco.com>
2023-02-21 23:36:51 +0100

    feat: testing is auth middleware

--- src/pages/api/matches.ts
+++ src/pages/api/matches.ts
@@ -2,11 +2,37 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { AirtableData } from '@/database/Airtable';
 import { MatchAirtableRepository } from '@/modules/matches/repositories/MatchAirtableRepository';
 import { ResponseMatchData, ResponseSingleMatchData } from '@/modules/matches/model';
+import { SessionAirtableRepository } from '@/modules/sessions/repositories/SessionAirtableRepository';
+import { Session } from '@/modules/sessions/model';
+
+const isAuthenticated = async <T>(req: NextApiRequest, res: NextApiResponse<T>): Promise<Session | undefined> => {
+  const token = req.cookies.token;
+  if (!token) {
+    return undefined;
+  }
+
+  const sessionRepository = new SessionAirtableRepository(new AirtableData());
+  try {
+    const t = new Date().getTime();
+    const ses = await sessionRepository.getById(token);
+    console.log((new Date().getTime() - t) / 1000);
+    return ses;
+  } catch (error: any) {
+    return undefined;
+  }
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseMatchData | ResponseSingleMatchData>
 ) {
+  const session = await isAuthenticated<ResponseMatchData | ResponseSingleMatchData>(req, res);
+  if (!session) {
+    return res.status(401).json({
+      success: false,
+    });
+  }
+
   const matchRepository = new MatchAirtableRepository(new AirtableData());
 
   if (req.method === 'POST') {
