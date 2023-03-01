1f12ef0d9ccfa410b47b624b6c31911d1ab94aa8
Marc <marc@hosco.com>
2023-03-01 23:29:16 +0100

    fix: some styling

--- src/modules/matches/components/MatchItem/MatchTime.tsx
+++ src/modules/matches/components/MatchItem/MatchTime.tsx
@@ -1,14 +1,14 @@
 import { format, addSeconds } from '@/modules/common/services/dates';
-import { Paragraph } from './styles';
+import { Text } from './styles';
 
 const MatchTime = ({ startTime, duration }: { startTime: string; duration: number }) => {
   return (
-    <Paragraph>
+    <Text>
       {`De `}
       {format(startTime, 'H:mm')}
       {` a `}
       {format(addSeconds(new Date(startTime), duration).toString(), 'H:mm')}
-    </Paragraph>
+    </Text>
   );
 };
 
--- src/modules/matches/components/MatchItem/styles.ts
+++ src/modules/matches/components/MatchItem/styles.ts
@@ -3,11 +3,11 @@ import styled from 'styled-components';
 export const Paragraph = styled.p`
   font-size: 1.5rem;
   text-transform: capitalize;
-  margin: 0.2rem;
+  margin: 0.2rem 0;
 `;
 
 export const Text = styled.span<{ color?: string }>`
-  font-size: 1.4rem;
+  font-size: 1.5rem;
   margin-right: 0.5rem;
   color: ${({ color }) => color || 'inherit'};
 `;
