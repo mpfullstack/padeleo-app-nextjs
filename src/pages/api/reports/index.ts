cf271e8da14504a8aed0271b03323ebf89871f3a
Marc <marc@hosco.com>
2023-09-02 19:54:14 +0200

    fix: set csv headers and correct lost matches

--- src/pages/api/reports/index.ts
+++ src/pages/api/reports/index.ts
@@ -8,31 +8,27 @@ import { User } from '@/modules/users/model';
 import { AirtableReporter } from '@/modules/reports/services/report';
 import { CSVExport } from '@/modules/reports/services/CSVExport';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseReportData>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseReportData | string>) {
   const session = await getSession(req);
 
   if (!session || !session.user?.admin) return res.status(401).json({ success: false });
 
-  // Download report
   if (req.method === 'GET') {
     const airtableData = new AirtableData();
     const userRepository = new UserAirtableRepository(airtableData);
     const matchRepository = new MatchAirtableRepository(airtableData);
 
     try {
-      const users = await userRepository.getAll();
+      const users = (await userRepository.getAll()).filter(
+        (user: User) => !['Invitado1', 'Invitado2', 'Invitado3'].includes(user.nickname as string)
+      );
       const promises = users.map((user: User) => matchRepository.getByUserNickname(user.nickname as string));
       const matchesByUser = await Promise.all(promises);
       const reporter = new AirtableReporter(new CSVExport());
       const reportData = reporter.generate({ users, matchesByUser });
-      const reports = await reporter.export<any>(reportData);
+      const reports = await reporter.export<Promise<string>>(reportData);
 
       return res.status(200).setHeader('Content-Type', 'text/csv').send(reports);
-
-      // return res.status(200).json({
-      //   success: true,
-      //   result: reportData,
-      // });
     } catch (error: any) {
       return res.status(500).json({
         success: false,
